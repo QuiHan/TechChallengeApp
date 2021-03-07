b17a5e83eb9be8f0cd4b5248fed78e7d194df9d0
Qui Han <quihan.chew@quanton.co.nz>
2021-03-07 23:40:00 +1300

    remove old sql script

--- createtable.sql
+++
@@ -1,5 +0,0 @@
---CREATE DATABASE techappdb WITH OWNER = app ENCODING = 'UTF8' LC_COLLATE = 'en-US' LC_CTYPE = 'en-US' CONNECTION LIMIT = -1 TEMPLATE template0;
-CREATE TABLE tasks ( id SERIAL PRIMARY KEY, completed boolean NOT NULL, priority integer NOT NULL, title text NOT NULL);
-INSERT INTO tasks (completed, priority, title) VALUES(false, 0, '1st Task');
-INSERT INTO tasks (completed, priority, title) VALUES(false, 0, '2nd Task');
-INSERT INTO tasks (completed, priority, title) VALUES(false, 0, '3rd Task');
