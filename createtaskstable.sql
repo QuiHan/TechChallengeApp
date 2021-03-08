d5b536e05f040e4590b16e8b5f50791071bde1c0
Qui Han <quihan.chew@quanton.co.nz>
2021-03-08 18:22:40 +1300

    Changed seeding data

--- createtaskstable.sql
+++ createtaskstable.sql
@@ -1,5 +1,5 @@
 --CREATE DATABASE techappdb WITH OWNER = app ENCODING = 'UTF8' LC_COLLATE = 'en-US' LC_CTYPE = 'en-US' CONNECTION LIMIT = -1 TEMPLATE template0;
 CREATE TABLE tasks ( id SERIAL PRIMARY KEY, completed boolean NOT NULL, priority integer NOT NULL, title text NOT NULL);
-INSERT INTO tasks (completed, priority, title) VALUES(false, 0, '1st Task');
-INSERT INTO tasks (completed, priority, title) VALUES(false, 0, '2nd Task');
-INSERT INTO tasks (completed, priority, title) VALUES(false, 0, '3rd Task');
+INSERT INTO tasks (completed, priority, title) VALUES(false, 0, 'Thanks for taking the time to review my deployment');
+INSERT INTO tasks (completed, priority, title) VALUES(false, 0, 'Looking forward to hearing from you');
+INSERT INTO tasks (completed, priority, title) VALUES(false, 0, 'Q :)');
